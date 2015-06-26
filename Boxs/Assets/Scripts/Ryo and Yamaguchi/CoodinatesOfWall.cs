04ad25fe808fb51c73efe849665a062246ca3b76
tetsuyaguchi <ymgchi2015@gmail.com>
2015-06-26 19:10:42 +0900

    0626 yamaguchi 前方2段キューブの場合 fTroublePlayer()へ

--- Boxs/Assets/Scripts/Ryo and Yamaguchi/CoodinatesOfWall.cs
+++ Boxs/Assets/Scripts/Ryo and Yamaguchi/CoodinatesOfWall.cs
@@ -28,7 +28,7 @@ public class CoodinatesOfWall : MonoBehaviour {
 	{
 		if (ctl.frontFlg != RIGHT || ctl.frontFlg != LEFT) 
 		{
-			ctl.upFlg = WALL;
+			ctl.upFlg = BLOCK;
 		}
 		/*
 		if (player.GetComponent<PlayerControll> ().flag == BLOCK) 

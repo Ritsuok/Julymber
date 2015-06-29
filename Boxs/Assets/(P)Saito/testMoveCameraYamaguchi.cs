7345ae16c5be5bfa31b5cbde230bdd28163d8380
Psyla <akiraguwaguwa@gmail.com>
2015-06-29 20:51:56 +0900

    ゲームオーバーSceneの作成

---
+++ Boxs/Assets/(P)Saito/testMoveCameraYamaguchi.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testMoveCameraYamaguchi : MonoBehaviour {
+
+	public int rtt;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate (0, rtt, 0);
+	}
+}

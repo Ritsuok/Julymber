8c7ca4fab70884b06a41ad54980c5ad435a2e8e9
Psyla <akiraguwaguwa@gmail.com>
2015-07-02 16:58:11 +0900

    7/2(齋藤）

---
+++ Boxs/Assets/Scripts/RetryBtn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RetryBtn : MonoBehaviour {
+	private GameObject startBtn;
+	private StartBtn startbtnScript;
+	// Use this for initialization
+	void Start () {
+
+		
+		startBtn = GameObject.Find ("ButtonStart");
+		startbtnScript = startBtn.GetComponent<StartBtn> ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void fLetsRetry(){
+		startbtnScript.fStartBtnOn ();
+
+	}
+}
